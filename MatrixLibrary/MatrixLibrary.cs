c539a8ebab033a8d3f1019701ceb1b3c75df3e45
Kuryleva Anastasia <knastya76@yandex.ru>
2022-09-07 14:45:54 +0300

    Added files

---
+++ MatrixLibrary/MatrixLibrary.cs
@@ -0,0 +1,204 @@
+﻿
+
+namespace MatrixLibrary
+{
+	public class Matrix
+	{
+        private int n;
+        private int[,] array;
+
+        // Создаем конструкторы матрицы
+        public Matrix() { }
+        public int N
+        {
+            get { return n; }
+            set { if (value > 0) n = value; }
+        }
+
+        //Для работы вне класса
+        public Matrix(int n)
+        {
+            this.n = n;
+            array = new int[this.n, this.n];
+        }
+        public int this[int i, int j]
+        {
+            get
+            {
+                return array[i, j];
+            }
+            set
+            {
+                array[i, j] = value;
+            }
+        }
+
+        // Ввод матрицы
+        public void MatrixIn()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.WriteLine("Введите элемент матрицы {0}:{1}", i + 1, j + 1);
+                    array[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+
+        public void MatrixInRandom()
+        {
+            var rand = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    this[i, j] = rand.Next() % 100;
+                }
+            }
+        }
+
+        // Вывод введенной матрицы
+        public void MatrixOut()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+
+        // Умножение матрицы на число
+        public static Matrix MultByNumber(Matrix a, int num)
+        {
+            Matrix result = new Matrix(a.N);
+            for (int i = 0; i < a.N; i++)
+            {
+                for (int j = 0; j < a.N; j++)
+                {
+                    result[i, j] = a[i, j] * num;
+                }
+            }
+            return result;
+        }
+
+        // Умножение матриц
+        public static Matrix MultByMatrix(Matrix a, Matrix b)
+        {
+            Matrix result = new Matrix(a.N);
+            for (int i = 0; i < a.N; i++)
+                for (int j = 0; j < b.N; j++)
+                    for (int k = 0; k < b.N; k++)
+                        result[i, j] += a[i, k] * b[k, j];
+
+            return result;
+        }
+
+
+
+        // перегрузка оператора умножения
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+            return Matrix.MultByMatrix(a, b);
+        }
+
+        public static Matrix operator *(Matrix a, int b)
+        {
+            return Matrix.MultByNumber(a, b);
+        }
+
+
+        // Метод вычитания матриц
+        public static Matrix Difference(Matrix a, Matrix b)
+        {
+            Matrix result = new Matrix(a.N);
+            for (int i = 0; i < a.N; i++)
+            {
+                for (int j = 0; j < b.N; j++)
+                {
+                    result[i, j] = a[i, j] - b[i, j];
+                }
+            }
+            return result;
+        }
+
+        // Перегрузка оператора вычитания
+        public static Matrix operator -(Matrix a, Matrix b)
+        {
+            return Matrix.Difference(a, b);
+        }
+
+        //Метод сложения матриц
+        public static Matrix Sum(Matrix a, Matrix b)
+        {
+            Matrix result = new Matrix(a.N);
+            for (int i = 0; i < a.N; i++)
+            {
+                for (int j = 0; j < b.N; j++)
+                {
+                    result[i, j] = a[i, j] + b[i, j];
+                }
+            }
+            return result;
+        }
+
+        // Перегрузка сложения
+        public static Matrix operator +(Matrix a, Matrix b)
+        {
+            return Matrix.Sum(a, b);
+        }
+
+        //Метод транспонирования матриц
+        public static Matrix Trans(Matrix a)
+        {
+            Matrix result = new Matrix(a.N);
+            for (int i = 0; i < a.N; i++)
+            {
+                for (int j = 0; j < a.N; j++)
+                {
+                    result[i, j] = a[j, i];
+                }
+            }
+            return result;
+        }
+
+        //Метод возведения в степень матриц
+        public static Matrix Pow(Matrix a, int b)
+        {
+            for (int i = 0; i < b; i++)
+            {
+                a *= a;
+                i++;
+            }
+            return a;
+        }
+
+        //Метод определения для матриц
+        public static int Det(Matrix a)
+        {
+            if (a.N == 2)
+            {
+                return a[0, 0] * a[1, 1] - a[0, 1] * a[1, 0];
+            }
+            if (a.N == 3)
+            {
+                return a[0, 0] * a[1, 1] * a[2, 2] + a[0, 1] * a[1, 2] * a[2, 0] +
+                a[0, 2] * a[1, 0] * a[2, 1] - a[0, 2] * a[1, 1] * a[2, 0] -
+                a[0, 0] * a[1, 2] * a[2, 1] - a[0, 1] * a[1, 0] * a[2, 2];
+            }
+            return 0;
+        }
+
+
+        // Деструктор Matrix
+        ~Matrix()
+        {
+            Console.WriteLine("Очистка");
+        }
+    }
+}
---
+++ lab1_new/Program.cs
@@ -0,0 +1,92 @@
+﻿using MatrixLibrary;
+
+class MainProgram
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите размерность матрицы: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+
+        Matrix mass1 = new Matrix(size);
+        Matrix mass2 = new Matrix(size);
+        
+
+
+        Console.WriteLine("1. Ввод вручную\n2. Автоматическое заполнение");
+        string answer = Console.ReadLine();
+
+            if (answer != null)
+            {
+                if (answer == "1")
+                {
+
+                    Console.WriteLine("ввод Матрица А: ");
+                    mass1.MatrixIn();
+
+                    Console.WriteLine("Ввод Матрица B: ");
+                    mass2.MatrixIn();
+
+                    Console.WriteLine("Матрица А: ");
+                    mass1.MatrixOut();
+
+                    Console.WriteLine("\nМатрица В: ");
+                    mass2.MatrixOut();
+                }
+                if (answer == "2")
+                {
+                    Console.WriteLine("Матрица А: ");
+                    mass1.MatrixInRandom();
+                    mass1.MatrixOut();
+
+                    Console.WriteLine("\nМатрица Б: ");
+                    mass2.MatrixInRandom();
+                    mass2.MatrixOut();
+                }
+            if (answer != "1" && answer != "2") { Console.WriteLine("Введен некорректный ответ!"); return; }
+            }
+        
+
+
+       // Инициализация
+       
+
+        
+
+        Console.WriteLine("\nСложение матриц А и Б: ");
+        Matrix resSum = new Matrix(size);
+        resSum = (mass1 + mass2);
+        resSum.MatrixOut();
+
+        Console.WriteLine("\nВычитание матриц А и Б: ");
+        Matrix resDiff = new Matrix(size);
+        resDiff = (mass1 - mass2);
+        resDiff.MatrixOut(); 
+        
+        Console.WriteLine("\nЧисло, на которое нужно умножить матрицу А: ");
+        Matrix resMultByNum = new Matrix(size);
+        resMultByNum = mass1 * Convert.ToInt32(Console.ReadLine());
+        resMultByNum.MatrixOut();
+
+        Console.WriteLine("\nУмножение матриц А и Б: ");
+        Matrix resMult = new Matrix(size);
+        resMult = (mass1 * mass2);
+        resMult.MatrixOut();
+
+        Console.WriteLine("\nТранспонирование матрицы А: ");
+        Matrix resTrans = new Matrix(size);
+        resTrans = Matrix.Trans(mass1);
+        resTrans.MatrixOut();
+
+        Console.WriteLine("\nЧисло, на которое нужно возвести в степень матрицу А: ");
+        Matrix resPow = new Matrix(size);
+        resMultByNum = Matrix.Pow(mass1, Convert.ToInt32(Console.ReadLine()));
+        resMultByNum.MatrixOut();
+
+        Console.WriteLine("\nОпределитель матрицы А: ");
+        int resDet = Matrix.Det(mass1);
+        Console.WriteLine(resDet);
+
+        return;
+    }
+}
